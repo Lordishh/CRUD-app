ccf9c288b7e7584232a0ec11403c61c2de5483f3
Enuel <enuel.monzon@gmail.com>
2021-11-05 23:25:01 -0300

    partials messages

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -43,11 +43,13 @@ router.get("/notes/edit/:id", async (req, res) => {
 router.put("/notes/edit-note/:id", async (req, res) => {
   const { title, description } = req.body;
   await Note.findByIdAndUpdate(req.params.id, { title, description }).lean();
+  req.flash("success_msg", "Nota actualizada satisfactoriamente");
   res.redirect("/notes");
 });
 
 router.delete("/notes/delete/:id", async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Nota borrada satisfactoriamente");
   res.redirect("/notes");
 });
 module.exports = router;
